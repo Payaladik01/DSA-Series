f1d01cb5bc674646a9a7c443b7a203ba327c4d10
Payaladik01 <payaladik267@gmail.com>
2026-08-03 21:18:25 +0530

    Selection sort

---
+++ selectionSort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+// Function to perform selection sort
+void selectionSort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        int minIndex = i; // Assume the current index has the minimum element
+        
+        // Find the index of the minimum element in the unsorted array
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        
+        // Swap the found minimum element with the first element
+        if(minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+int main() {
+    int n;
+ = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    // Calling the sorting function
+    selectionSort(arr, n);
+    
+    // Printing the sorted array
+    cout << "\nSorted array in ascending order:\n";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}    
+    // Taking input for the size of the array
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+    
+    // Taking input for the array elements
+    cout << "Enter " << n << " elements:\n";
+    for(int i
