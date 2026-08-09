ddd0b59d31f14b789214c7e380503d10702a169b
Payaladik01 <payaladik267@gmail.com>
2026-08-09 13:37:09 +0530

    Printing reversev array

---
+++ reverse_array.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+vector<int> reverse(vector<int> v){
+    int s = 0;
+    int e = v.size()-1;
+    while(s<=e){
+        swap(v[s], v[e]);
+        s++;
+        e--;
+    }
+    return v;
+}
+
+void print (vector<int> v){
+    for(int i=0; i<v.size();i++){
+        cout<<v[i]<<"";
+
+    }
+    cout<<endl;
+
+
+}
+int main(){
+
+    vector<int> v;
+    v.push_back(12);
+    v.push_back(18);
+    v.push_back(8);
+    v.push_back(4);
+    v.push_back(1);
+
+    vector<int> ans= reverse(v);
+    cout<<"Printing the reverse array:"<<endl;
+
+    print(ans);
+
+}
