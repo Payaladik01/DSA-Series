122b201cd5409953e0607b5237eb86d004ad5f99
Payaladik01 <payaladik267@gmail.com>
2026-08-14 18:52:37 +0530

    lect 21

--- mergeSortedArray.cpp
+++ mergeSortedArray.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-void merge(int arr1[],int n, int arr2[], int m, int arr3[]){
+void merge(int arr1[], int n , int arr2[], int m, int arr3[]){
     int i=0;
     int j=0;
     int k=0;
@@ -18,6 +18,7 @@ void merge(int arr1[],int n, int arr2[], int m, int arr3[]){
             j++;
         }
     }
+
     while(i<n){
             arr3[k]=arr1[i];
             k++;
@@ -27,21 +28,26 @@ void merge(int arr1[],int n, int arr2[], int m, int arr3[]){
             arr3[k]=arr2[j];
             k++;
             j++;
-        } 
+        }
+    
+
+    
 }
+
 void print(int arr[], int n){
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<"";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<""<<"";
+
     }
     cout<<endl;
 }
 int main(){
-    int arr1[5]={1,3,5,7,9};
-    int arr2[3]={2,4,6};
+    int arr1[4]={2,4,6,8};
+    int arr2[3]={3,5,7};
 
-    int arr3[8]={0};
+    int arr3[7]={0};
 
-    merge(arr1,5,arr2,3,arr3);
-    print(arr3,8);
+    merge(arr1,4,arr2,3,arr3);
+    print(arr3,7);
     return 0;
 }
